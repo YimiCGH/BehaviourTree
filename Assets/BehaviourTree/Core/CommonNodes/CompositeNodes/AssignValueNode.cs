912c3e20659b6e206e204c777e375abd1dd264a5
Yimi <455022370@qq.com>
2022-04-15 14:18:12 +0800

    添加赋值节点

---
+++ Assets/BehaviourTree/Core/CommonNodes/CompositeNodes/AssignValueNode.cs
@@ -0,0 +1,20 @@
+﻿namespace BT
+{
+    [Category("组合","赋值（AssignValueNode）")]
+    [NodeView("BT.AssignValueNodeView")]
+    public class AssignValueNode:CompositeNode
+    {
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override E_State OnUpdate()
+        {
+            return E_State.Success;
+        }
+    }
+}
